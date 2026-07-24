678206ce4df4cd525e13df0d05f1aa7c18159622
KevinYamilC <kevin34ke@gmail.com>
2026-07-24 00:10:08 -0600

    New Player size

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -19,7 +19,8 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
     super(scene, x, y, texture);
-
+    //this.setScale(0.5);
+    this.setDisplaySize(128, 128);
     this.hp = GAME_CONSTANTS.PLAYER_BASE_HP;
     this.maxHp = GAME_CONSTANTS.PLAYER_BASE_HP;
     this.level = 1;
